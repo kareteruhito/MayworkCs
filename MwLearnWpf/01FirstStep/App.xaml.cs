db466e945f45dbafc9f4047ee7a3149cd92d354a
kareteruhito <kareteru.hito@yahoo.co.jp>
2026-01-24 07:43:19 +0900

    巨大なバイナリファイルを除外してコミット

---
+++ MwLearnWpf/01FirstStep/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+// WPF の基本クラス（Application / Window など）が定義されている名前空間
+
+namespace _01FirstStep;
+// このアプリケーション全体の名前空間
+// App.xaml の x:Class="_01FirstStep.App" と対応している
+
+public partial class App : Application
+// アプリケーション本体を表すクラス
+// Application クラスを継承することで、WPF アプリとして動作する
+// partial は App.xaml 側で定義された内容と結合されることを意味する
+{
+    // このクラスには、アプリ起動・終了などのイベント処理を記述できる
+    //
+    // 例:
+    //  - OnStartup()   : アプリ起動時
+    //  - OnExit()      : アプリ終了時
+    //  - DispatcherUnhandledException : 例外の一括捕捉
+    //
+    // 今回は特別な処理が無いため空クラスになっている    
+}
+
---
+++ MwLearnWpf/01FirstStep/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+// WPF の基本クラス（Window / Application など）が定義されている名前空間
+
+namespace _01FirstStep;
+// このクラスが属する名前空間
+// MainWindow.xaml の x:Class="_01FirstStep.MainWindow" と対応している
+
+public partial class MainWindow : Window
+// メインウィンドウを表すクラス
+// Window クラスを継承することで「画面」として動作する
+// partial は XAML 側で定義された内容と結合されることを意味する
+{
+    public MainWindow()
+    {
+        // コンストラクタ（ウィンドウ生成時に最初に呼ばれる）
+
+        InitializeComponent();
+        // MainWindow.xaml に記述された XAML を読み込み、
+        // コントロールを生成して画面に配置する自動生成メソッド
+        //
+        // 内部的には以下のような処理が行われている：
+        //
+        //  - XAML の解析
+        //  - Button / Grid などのインスタンス生成
+        //  - Name 指定されたコントロールのフィールド生成
+        //  - イベントハンドラの接続
+        //
+        // この呼び出しを削除すると、画面は空の Window になります。
+    }
+}
